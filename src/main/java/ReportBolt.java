e5917a7d0022855a9fb7185da158c174da8b2f53
Qu Yuan <370559301@qq.com>
2017-06-13 13:12:35 +0800

    final version

---
+++ src/main/java/ReportBolt.java
@@ -0,0 +1,39 @@
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+
+import java.util.*;
+
+/**
+ * Created by qy on 2016/10/23.
+ */
+public class ReportBolt extends BaseRichBolt
+{
+    private HashMap<String, Long> counts = null;
+
+    public void prepare(Map config, TopologyContext context, OutputCollector collector) {
+        this.counts = new HashMap<String, Long>();
+    }
+    public void execute(Tuple tuple) {
+        String word = tuple.getStringByField("word");
+        Long count = tuple.getLongByField("count");
+        this.counts.put(word, count);
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        // this bolt does not emit anything
+    }
+    public void cleanup() {
+        System.out.println("--- FINAL COUNTS ---");
+        List<String> keys = new ArrayList<String>();
+        keys.addAll(this.counts.keySet());
+        Collections.sort(keys);
+        for (String key : keys) {
+            System.out.println(key + " : " + this.counts.get(key));
+        }
+        System.out.println("--------------");
+    }
+
+}
